03c89653d0c42c5c81cf56233287ffb7dad87ae0
Matthijs Arnoldus <mat.arnoldus@kpnmail.nl>
2024-08-16 13:43:22 +0200

    Add plans

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -6,6 +6,30 @@ export default function Contact() {
         <p className="text-2xl md:text-4xl my-4 md:my-8">Choose the <span className="text-orange font-bold">plan</span> that suits you!</p>
         <div className="flex flex-col md:flex-row gap-8 md:gap-16 justify-center w-full px-2 md:px-8">
           <div className="flex flex-col rounded-md bg-grey text-center p-4 shadow-xl border-2 border-blue-darkest w-full md:w-1/2">
+            <p className="text-3xl md:text-4xl font-bold pb-4">In-person & online technique coaching</p>
+            <p className="text-lg md:text-xl pb-4"><span className="text-orange text-3xl md:text-4xl font-bold">€135</span>/4 weeks</p>
+            <p className="text-sm md:text-md">Every 4 weeks a 1 hour person training with one of our coaches combined with 3 weeks of digital feedback based on what you have worked on. A travel cost might be charged</p>
+            <a href="mailto:info@personalonlineswimcoach.com" className="bg-orange min-w-fit rounded p-2 md:p-4 text-white text-lg md:text-xl shadow-md my-4 md:my-8">Sign up</a>
+            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Combine in-person and online coaching</p>
+            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Individual training to identify and practice new technique goals</p>
+            <p className="text-lg md:text-xl py-2 md:py-4">Weekly in-depth analysis of your progress</p>
+          </div>
+          
+          <div className="flex flex-col rounded-md bg-grey text-center p-4 shadow-xl w-full md:w-1/2 border-2 border-blue-darkest">
+            <p className="text-3xl md:text-4xl font-bold pb-4">Online Individual Coaching</p>
+            <p className="text-lg md:text-xl pb-4"><span className="text-orange text-3xl md:text-4xl font-bold">€10 - €400</span>/4 weeks</p>
+            <div className="flex-grow"></div>
+            <p className="text-sm md:text-md">Online individual coaching is fully tailored to your needs. The price depends on how often you train and how detailed you like your training plans. In the free intake the price is calculated based on your needs.</p>
+            
+            <a href="mailto:info@personalonlineswimcoach.com" className="bg-orange min-w-fit rounded p-2 md:p-4 text-white text-lg md:text-xl shadow-md my-4 md:my-8">Sign up</a>
+            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Free online intake to discuss your goals</p>
+            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Every week new plans for your training sessions</p>
+            <p className="text-lg md:text-xl py-2 md:py-4">Weekly feedback on training results and monthly evaluation of goals</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-8 md:gap-16 justify-center w-full px-2 md:px-8 mt-16">
+          <div className="flex flex-col rounded-md bg-grey text-center p-4 shadow-xl border-2 border-blue-darkest w-full md:w-1/2">
             <p className="text-3xl md:text-4xl font-bold pb-4">Online Technique Coaching</p>
             <p className="text-lg md:text-xl pb-4"><span className="text-orange text-3xl md:text-4xl font-bold">€50</span>/4 weeks</p>
             <p className="text-sm md:text-md">Or get a discount for booking multiple months in advance:</p>
@@ -18,13 +42,14 @@ export default function Contact() {
           </div>
           
           <div className="flex flex-col rounded-md bg-grey text-center p-4 shadow-xl w-full md:w-1/2 border-2 border-blue-darkest">
-            <p className="text-3xl md:text-4xl font-bold pb-4">Online Individual Coaching</p>
-            <p className="text-lg md:text-xl pb-4"><span className="text-orange text-3xl md:text-4xl font-bold">€10 - €400</span>/4 weeks</p>
-            <p className="text-sm md:text-md">Online individual coaching is fully tailored to your needs. The price depends on how often you train and how detailed you like your training plans. In the free intake the price is calculated based on your needs.</p>
+            <p className="text-3xl md:text-4xl font-bold pb-4">Individual Coaching</p>
+            <p className="text-lg md:text-xl pb-4"><span className="text-orange text-3xl md:text-4xl font-bold">€100</span>/hour</p>
+            <div className="flex-grow"></div>
+            <p className="text-sm md:text-md">Based on individual needs and availability location and time of the sessions will be discussed. A travel cost might be charged.</p>
             <a href="mailto:info@personalonlineswimcoach.com" className="bg-orange min-w-fit rounded p-2 md:p-4 text-white text-lg md:text-xl shadow-md my-4 md:my-8">Sign up</a>
-            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Free online intake to discuss your goals</p>
-            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Every week new plans for your training sessions</p>
-            <p className="text-lg md:text-xl py-2 md:py-4">Weekly feedback on training results and monthly evaluation of goals</p>
+            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Individual technique session at the pool</p>
+            <p className="text-lg md:text-xl border-b-2 py-2 md:py-4">Working one-on-one with our expert coaches</p>
+            <p className="text-lg md:text-xl py-2 md:py-4">Personal advice on continuing your practice</p>
           </div>
         </div>
         
